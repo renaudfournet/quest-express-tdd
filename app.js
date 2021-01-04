320f6a4a158012b3fdf826b52a80bbab55b031ac
renaudfournet <fournet.renaud@gmail.com>
2021-01-04 12:14:07 +0100

    add other test and route exercice

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+const connection = require("./connection");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +10,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   res.status(200).json({ message: "Hello World!" });
+});
+
+app.post("/bookmarks", (req, res) => {
+  const { url, title } = req.body;
+  if (!url || !title) {
+    return res.status(422).json({ error: "required field(s) missing" });
+  }
+  connection.query("INSERT INTO bookmark SET ?", req.body, (err, stats) => {
+    if (err) return res.status(500).json({ error: err.message, sql: err.sql });
+
+    connection.query(
+      "SELECT * FROM bookmark WHERE id = ?",
+      stats.insertId,
+      (err, records) => {
+        if (err)
+          return res.status(500).json({ error: err.message, sql: err.sql });
+        return res.status(201).json(records[0]);
+      }
+    );
+  });
 });
 
 module.exports = app;
