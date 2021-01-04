7efc6ba0477d8d276eb10ce211d140558fb743c3
renaudfournet <fournet.renaud@gmail.com>
2021-01-04 10:20:15 +0100

    first test get

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.status(200).json({ message: "Hello World!" });
+});
 
 module.exports = app;
